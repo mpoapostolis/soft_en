7009fad7940977fb92d5025caa62624bdad81bf2
mpoapostolis <mpoapostolis@gmail.com>
2017-12-18 15:11:33 +0200

    ci/cd: test2

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+echo running as mpoapostolis;
+sudo su mpoapostolis;
 echo remove old build;
 rm -rf build;
 echo pull the new origin;
@@ -5,5 +7,5 @@ git pull;
 echo build new static files;
 npm run build;
 echo sync nginx static files;
-rsync -az build/ /var/www/html/
+sudo rsync -az build/ /var/www/html/
 echo done;
