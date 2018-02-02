7aa3fd4b8ef2c866a748c2a30b85d25b8ca6ff49
mpoapostolis <mpoapostolis@gmail.com>
2018-02-02 13:31:09 +0200

    Added DB associations

--- api/db/index.js
+++ api/db/index.js
@@ -7,7 +7,9 @@ const Books = require('./models/books')
 const Listing = require('./models/listing')
 const Activity = require('./models/activity')
 const Tag = require('./models/tag')
+const Review = require('./models/review')
 
+// Initialize DB handler.
 const sequelize = new Sequelize(
     config.database,
     config.username,
@@ -15,6 +17,7 @@ const sequelize = new Sequelize(
     config.options
 )
 
+// Define DB Entity models.
 const user = User(sequelize)
 const parent = Parent(sequelize)
 const owner = Owner(sequelize)
@@ -22,6 +25,20 @@ const books = Books(sequelize)
 const listing = Listing(sequelize)
 const activity = Activity(sequelize)
 const tag = Tag(sequelize)
+const review = Review(sequelize)
+
+// Define associations.
+owner.belongsTo(user, {foreignKey: 'OwnerID'})
+parent.belongsTo(user, {foreignKey: 'ParentID'})
+review.belongsTo(books, {foreignKey: 'BookingID'})
+
+owner.hasMany(activity, {foreignKey: 'OwnerID'})
+activity.hasMany(listing, {foreignKey: 'ActivityID'})
+
+listing.hasMany(books, {foreignKey: 'ListingID'})
+parent.hasMany(books, {foreignKey: 'ParentID'})
+
+activity.hasMany(tag, {foreignKey: 'ActivityID'})
 
 module.exports = {
     sequelize,
