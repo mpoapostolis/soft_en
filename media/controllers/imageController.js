97a5632037ddf3b19f23906a3f1d2c2a7c5522a5
mpoapostolis <mpoapostolis@gmail.com>
2018-02-10 22:01:19 +0200

    Revised SRS, added endpoint definitions

--- media/controllers/imageController.js
+++ media/controllers/imageController.js
@@ -1,7 +1,15 @@
 // Global image specifications.
 // TODO configure for multiple image sizes.
+// TODO Store info in a config.json file.
 const height = 450
 const width = 600
+
+const bgColor = 0x0000ffff
+const bgWidth = 400
+const bgHeight = 90
+const margin = 10
+
+const destinationDirectory = 'images/'
 
 const Jimp = require('jimp')
 const multer = require('multer')
@@ -12,7 +20,7 @@ const mime = require('mime')
 // extension.
 const storage = multer.diskStorage({
     destination: (req,res,cb) => {
-        cb(null, 'images/')
+        cb(null, destinationDirectory)
     },
     filename: (req,file,cb) => {
         crypto.pseudoRandomBytes(6, (err,raw) => {
@@ -25,19 +33,34 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage: storage})
 
+function captionBackground(color,width,height) {
+    return function(x,y,offset) {
+        if(2*y+9*x<=9*width) {
+            this.bitmap.data.writeUInt32BE(color, offset, true);
+        }
+    }
+}
+
+// TODO Add a caption background size calculator, based on caption word length.
+const blueBackground = captionBackground(bgColor,bgWidth,bgHeight)
+
 // Image processing function, it returns a promise with the generated file name.
 function process(destination, filename ,caption) {
     var filepath = destination + filename
     return Jimp.read(filepath)
         .then( (image) => {
-            return Jimp.loadFont(Jimp.FONT_SANS_32_BLACK)
-            .then( (font) => {
-                image.resize(Jimp.AUTO,height)
-                     .cover(width,height,Jimp.HORIZONTAL_ALIGN_CENTER,Jimp.VERTICAL_ALIGN_MIDDLE)
-                     .print(font,10,10,caption,width/2)
-                     .write(filepath);
-                return Promise.resolve(filename)
-            })
+            return Jimp.loadFont(Jimp.FONT_SANS_32_WHITE)
+                       .then( (font) => {
+                           return Promise.resolve([image,font])
+                       })
+        })
+        .then( ([image,font]) => {
+            image.resize(Jimp.AUTO,height)
+                 .cover(width,height,Jimp.HORIZONTAL_ALIGN_CENTER,Jimp.VERTICAL_ALIGN_MIDDLE)
+                 .scan(0,0,bgWidth,bgHeight,blueBackground)
+                 .print(font,margin,margin,caption,bgWidth-2*margin)
+                 .write(filepath);
+            return Promise.resolve(filename)
         })
         .catch( (err) => {
             console.error(err)
