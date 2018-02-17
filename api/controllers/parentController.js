b4bc62839e4feba3edfbb03439bfe9224311d91b
mpoapostolis <mpoapostolis@gmail.com>
2018-02-17 11:30:38 +0200

    WIP: Parent GET /api/wallet

--- api/controllers/parentController.js
+++ api/controllers/parentController.js
@@ -1,64 +1,64 @@
 function parentController(app, db) {
 
-  app.post('/wallet', app.loggedIn, (req, res) => {
-      // Initiate transaction.
-      db.sequelize.transaction( (t) => {
-          // Lock parent table row.
-          return db.parent.findById(
-              req.headers.UserID,
-              {
-                  transaction: t,
-                  lock: t.LOCK.UPDATE
-              }
-          ).then((p) => {
-              // Ensure top up amount is a valid number.
-              let topup = parseInt(req.body.Amount) || 0
-              topup = ( topup > 0 ) ? topup : 0
-
-              // Top up account balance.
-              p.Balance += topup
-
-              // And commit transaction.
-              return p.save({ transaction: t }).then(() => {
-                  res.send("Successful balance top up.")
-              })
-          })
-      })
-  })
-
-  app.get('/wallet', app.loggedIn, (req, res) => {
-      // TODO Respond according to spec.
-    console.log(req.headers.UserID)
-    db.parent.findById(req.headers.UserID, {
-      include: [
-        {
-          model: db.books,
-          as: 'books',
-          include: [
-            {
-              model: db.listing,
-              as: 'listing',
-              include: [
-                {
-                  model: db.activity,
-                  attributes: ['Name'],
-                  as: 'activity'
+    app.post('/wallet', app.loggedIn, (req, res) => {
+        // Initiate transaction.
+        db.sequelize.transaction((t) => {
+            // Lock parent table row.
+            return db.parent.findById(
+                req.headers.UserID, {
+                    transaction: t,
+                    lock: t.LOCK.UPDATE
                 }
-              ]
+            ).then((p) => {
+                // Ensure top up amount is a valid number.
+                let amount = parseInt(req.body.Amount) || 0
+                amount = (amount > 0) ? amount : 0
+
+                // Top up account balance.
+                p.Balance += amount
+
+                // And commit transaction.
+                return p.save({
+                    transaction: t
+                }).then(() => {
+                    res.send("Successful balance top up.")
+                })
+            })
+        })
+    })
+
+    app.get('/wallet', app.loggedIn, (req,res) => {
+        var response = {}
+        db.parent.findById(
+            req.headers.UserID, {
+                attributes: [ "Balance", "BonusPoints" ]
             }
-          ]
-        }
-      ]
+        ).then((r) => {
+            Object.assign(response,r.dataValues)
+
+            // TODO Add options for result pagination.
+            db.books.findAll({
+                where: {
+                    ParentID: req.headers.UserID
+                },
+                attributes: ["Quantity"],
+                include: [{
+                    model: db.listing,
+                    as: 'listing',
+                    attributes: ["EventDate"],
+                    include: {
+                        model: db.activity,
+                        as: 'activity',
+                        attributes: ["Name"]
+                    }
+                }]
+            }).then( (r) => {
+                // TODO Restructure response according to spec.
+                Object.assign(response,{ "bookings": r })
+                res.send(response)
+            })
+        })
     })
-    .then( (r) => {
-      res.send(r)
-    })
-/*
-    db.sequelize.query("SELECT 'Books.Quantity', 'Listing.EndDate', 'Listing.Price', 'Ativity.Name' FROM Books INNER JOIN Listing ON 'Books.ListingID'='listing.ListingID' INNER JOIN Activity ON 'Listing.ActivityID'='Activity.ActivityID' WHERE 'Books.ParentID'='4'", { type: db.sequelize.QueryTypes.SELECT}).then(history => {
-      console.log(history)
-      res.send(history);
-    })*/
-  })
 }
 
 module.exports = parentController
