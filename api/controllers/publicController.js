5b26aec2f10b144f837b0b8816af2d4d4acfd53e
mpoapostolis <mpoapostolis@gmail.com>
2018-02-17 22:27:35 +0000

    Updated publicController.js

--- api/controllers/publicController.js
+++ api/controllers/publicController.js
@@ -7,13 +7,41 @@ function publicController(app, db) {
 
     app.get('/activity/:id', (req, res) => {
         // TODO Optimize response according to spec.
+        var response = {}
         db.activity.findById(req.params.id, {
-            include: [{
-                // Add options for listing pagination.
-                model: db.listing
-            }]
-        }).then((result) => {
-            res.send(result)
+            attributes: [
+                ["Name","ActivityName"],
+                "Description",
+                "Price",
+                "Duration"
+            ],
+            include: [
+                {
+                    model: db.owner,
+                    attributes: ["CompanyName","Address"]
+                }
+            ]
+        }).then( (result) => {
+            Object.assign(response, result.dataValues)
+            db.listing.findAll({
+                where: {
+                    ActivityID: req.params.id,
+                    EventDate: {
+                        // Ignore past listings.
+                        [db.sequelize.Op.gt]: new Date()
+                    },
+                    Remaining: {
+                        // Ignore listings without remaining tickets.
+                        [db.sequelize.Op.gt]: 0
+                    }
+                },
+                attributes: { exclude: ["ActivityID"]}
+            }).then( (result) => {
+                Object.assign(response,{ "Listings": result })
+                res.send(response)
+            })
+        }).catch( (err) => {
+            res.send('No activity with uuid: ' + req.params.id)
         })
     })
 }
