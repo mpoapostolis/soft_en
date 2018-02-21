6504c1d51bea7a4d35c96cd116762792ccf0244d
mpoapostolis <mpoapostolis@gmail.com>
2018-02-21 11:41:16 +0200

    Revert "User search methods added"
    This reverts commit b3e702a702b89f937db1a03a68ed6cc3896252c7.

--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -3,19 +3,15 @@ function adminController(app,db) {
     app.get('/admin/user/', app.loggedIn, app.isAdmin, (req,res) => {
         db.user.findAll()
         .then( (users) => {
-            res.status(200).send(users)
-        }).catch((err) => {
-            res.status(404).send('Any user found: ' + err)
-        }
+            res.send(users)
+        })
     })
 
     app.get('/admin/user/:UserID', app.loggedIn, app.isAdmin, (req,res) => {
         db.user.findById(req.params.UserID)
         .then( (user) => {
-            res.status(200).send(users)
-        }).catch((err) => {
-            res.status(404).send('User not found: ' + req.params.UserID + " || " + err)
-        }
+            res.send(users)
+        })
     })
 
 }
