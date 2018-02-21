ae8fe1fb087838f28175734bbdf3d561834453ca
mpoapostolis <mpoapostolis@gmail.com>
2018-02-21 10:25:44 +0200

    Small invoice fix

--- mail/test.js
+++ mail/test.js
@@ -5,10 +5,12 @@ const data = {
     "CustomerName": 'Kostas Karamanlis',
     "Email": "straw.leaves@gmail.com",
     "Date": "2018-04-01",
+    "total": 400,
     "tickets": [
         {
             ticketNo: 324323,
             activityName: 'Karagounis Champions Soccer Academy',
+            EventDate: new Date(),
             price: 100,
             quantity: 2,
             total: 200
@@ -16,6 +18,7 @@ const data = {
         {
             ticketNo: 465342,
             activityName: 'Nakas Choir',
+            EventDate: new Date(),
             price: 100,
             quantity: 2,
             total: 200
